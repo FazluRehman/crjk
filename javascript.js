19437d03c4dec73262039cd66b06bb98db5863f6
FazluRehman <123297773+FazluRehman@users.noreply.github.com>
2023-01-23 23:25:06 -0800

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,13 @@
+
+<script>
+    document in html(
+      function navbar
+      
+      var x = document.getElementById("myTopnav");(
+      if (x.className === "topnav") )
+      x.className += " responsive";(
+      ) else 
+      x.className = "topnav";
+      )
+      
+       </script>   
